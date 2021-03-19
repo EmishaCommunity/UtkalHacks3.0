26aac86a5f670634f212625a38ec454da298c058
catchsahil <sahilsahilkumar1234@gmail.com>
2021-03-20 01:05:25 +0530

    Speaker added

--- component/SpeakerSection.js
+++ component/SpeakerSection.js
@@ -8,10 +8,9 @@ const speakersData = [
     id: "1",
     name: "Vilva Athiban P B",
     title: "Senior JavaScript Developer",
-    company: "Omio",
+    company: "@Omio",
     profilepic: "https://vilvaathiban.com/intro.jpg",
     linkedin: "https://linkedin.com/in/vilvaathiban",
-
     github: "https://github.com/vilvaathibanpb",
     twitter: "https://twitter.com/vilvaathibanpb",
     website: "https://vilvaathiban.com"
@@ -20,7 +19,7 @@ const speakersData = [
     id: "2",
     name: "Michael Riddering",
     title: "Building FigmaAcademy",
-    company: "ShiftNudge",
+    company: "@ShiftNudge",
     profilepic: "https://pbs.twimg.com/profile_images/1345910398722125827/rt_vhp2A_400x400.png",
     linkedin: "https://linkedin.com/in/michaelriddering",
     dribbble: "https://dribbble.com/riddering",
@@ -31,7 +30,7 @@ const speakersData = [
     id: "3",
     name: "Darshan Gajara",
     title: "Design Lead",
-    company: "GraphCMS",
+    company: "@GraphCMS",
     profilepic: "https://pbs.twimg.com/profile_images/1117472858836434944/FbWce7CZ_400x400.jpg",
     dribbble: "https://dribbble.com/WeirdoWizard",
     twitter: "https://twitter.com/WeirdoWizard",
@@ -39,26 +38,56 @@ const speakersData = [
   },
   {
     id: "4",
-    name: "Jamie Barton",
-    title: "Developer Experience Engineer",
-    company: "GraphCMS",
-    profilepic: "https://pbs.twimg.com/profile_images/1353655926943870976/bz18ljyJ_400x400.jpg",
-
-    linkedin: "https://linkedin.com/in/notrab",
-    github: "https://github.com/notrab",
-    twitter: "https://twitter.com/notrab",
-    website: "https://notrab.dev"
+    name: "Scott Spence",
+    title: "web developer",
+    company: "@karmarama",
+    profilepic: "https://res.cloudinary.com/emishalabs/image/upload/v1616179877/UtkalHacks%203.0/ScottSpence_ayssuh.png",
+    github: "https://github.com/spences10",
+    twitter: "https://twitter.com/spences10",
+    website: "https://scottspence.com/"
   },
   {
     id: "5",
     name: "Johan Eliasson",
     title: "Founder",
-    company: "NHost",
+    company: "@NHost",
     profilepic: "https://pbs.twimg.com/profile_images/1237013895375728640/F-aV_86L_400x400.jpg",
     linkedin: "https://linkedin.com/in/johan--eliasson",
     github: "https://github.com/elitasson",
     twitter: "https://twitter.com/elitasson"
-  }
+  },
+  {
+    id: "6",
+    name: "Tadit Dash",
+    title: "Software Engineer | Author",
+    company: "Prev. Software Engineer @Mindfire Solutions",
+    profilepic: "https://res.cloudinary.com/emishalabs/image/upload/v1616178958/UtkalHacks%203.0/Tadit_jnv2dy.jpg",
+    linkedin: "https://www.linkedin.com/in/taditdash/",
+    github: "https://github.com/taditdash",
+    twitter: "https://twitter.com/taditdash",
+    website: "http://taditdash.co.in/"
+  },
+  {
+    id: "7",
+    name: "Anshuman Verma",
+    title: "Software Engineer",
+    company: "@clarisights",
+    profilepic: "https://res.cloudinary.com/emishalabs/image/upload/v1616180653/UtkalHacks%203.0/Anshuman_hewff8.jpg",
+    linkedin: "https://www.linkedin.com/in/anshumanv12/",
+    github: "https://github.com/anshumanv",
+    twitter: "https://twitter.com/anshumaniac",
+    website: "https://anshumanv.dev/"
+  },
+  {
+    id: "8",
+    name: "Sayli Patil",
+    title: "Tech Lead",
+    company: "The Dapp List",
+    profilepic: "https://res.cloudinary.com/emishalabs/image/upload/v1616181034/UtkalHacks%203.0/sayli_kb160f.jpg",
+    linkedin: "https://www.linkedin.com/in/ptsayli/",
+    twitter: "https://twitter.com/ptsayli"
+  },
+
 ]
 
 const SpeakerSection = () => {
